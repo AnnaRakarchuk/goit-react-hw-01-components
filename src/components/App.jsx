a816929d06e2045c5742f406596e65ebd8130860
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-10-23 23:12:42 +0300

    Add task FriendList and TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import profile from './jsonFiles/user.json';
 import data from './jsonFiles/data.json';
+import friends from './jsonFiles/friends.json'
+import transactions from './jsonFiles/transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import {TransactionHistory} from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -16,6 +20,8 @@ export const App = () => {
     likes={profile.stats.likes}
   />
    <Statistics title="Upload stats" stats={data} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
     </div>
   );
 };
